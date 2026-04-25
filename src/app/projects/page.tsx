a966039020691dc9c7b604df4c5a825ba5f0dfa2
Denis Parada <denispar.dev@gmail.com>
2026-04-24 23:21:21 -0300

    refactor: remove mock projects, use Contentful as sole data source

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,9 @@
 import { Suspense } from "react";
 import { ProjectsSection } from "@/components/projects/ProjectsSection";
 import { getProjects } from "@/lib/contentful";
-import { projects as fallbackProjects } from "@/data/projects";
 
 export default async function ProjectsPage() {
-  let projects;
-  try {
-    projects = await getProjects();
-  } catch(error) {
-    console.error(error);
-    projects = fallbackProjects;
-  }
+  const projects = await getProjects();
 
   return (
     <main>
